1c84c1cd0f6d4c334a6f1c946513d9cb552d8606
Enrique <edavalos@ucsc.edu>
2020-07-20 00:49:30 -0700

    Finished interface

--- src/com/edavalos/Crypto/Chain.java
+++ src/com/edavalos/Crypto/Chain.java
@@ -1,5 +1,39 @@
 package com.edavalos.Crypto;
 
-public interface Chain {
+public interface Chain<T> {
+
+    // Seals current block and creates new one. Returns false if failed
+    boolean nextBlock();
+
+    // Adds something to the current block. Returns false if failed
+    boolean add(T item);
+    boolean add(T[] items);
+
+    // Returns number of blocks
+    int size();
+
+    // Returns total number of items in every block
+    int totalItems();
+
+    // Discards current block and everything in it, and creates a new one to replace it
+    void discardBlock();
+
+    // If item is in this chain, returns the first block id of which it is found. Otherwise returns -1
+    int contains(T item);
+
+    // Returns a list of every item in block of specified id. Returns null if id is out of range
+    T getContents(int blockID);
+
+    // Checks if current block is empty
+    boolean isCurrentEmpty();
+
+    // Checks if entire chain is empty
+    boolean isEmpty();
+
+    // Converts entire chain to a 2D array
+    T[][] toArray();
+
+    // Visualizes chain
+    String toString();
 
 }
